69a9478d562d36efefe8ddea6fa7c4db38cc4e29
Rob Audenaerde <rob.audenaerde@valuecare.nl>
2025-09-29 13:37:55 +0200

    betere code

--- tasks.py
+++ tasks.py
@@ -10,16 +10,13 @@ def coolblue_voorbeeld():
     """
     Voorbeeld robot: open coolblue, zoek de iphone 17 en maak een screenshot!
     """
-    browser.configure(
-        browser_engine="chromium",
-        screenshot="only-on-failure",
-        headless=False,
-    )
+    browser.configure( browser_engine="chromium", screenshot="only-on-failure", 
+                       headless=False,)
     try:
         page = browser.page()
         page.goto("https://www.coolblue.nl")
 
-        # (Optional) handle cookie banner if it appears
+        # Handle cookie banner if it appears
         try:
             cookie_button = page.locator("button[name='accept_cookie']").first
             cookie_button.wait_for(state="visible", timeout=3000)
@@ -27,24 +24,22 @@ def coolblue_voorbeeld():
         except Exception as e:
             log.info (e)
 
-        # Locate the first input inside div.aap (your example used a different selector;
-        # for Coolblue search, use their search input)
+        # Locator voor het "query" <input> element  
         search = page.locator("input[name='query']")
 
-        log.info(search)
-
+        # Wacht (max 3 sec) tot het zichtbaar is
         search.wait_for(state="visible", timeout=3000)
 
         # Type text and press Enter
         search.fill("iphone 17")
         search.press("Enter")
 
+        # Locator voor de "eerste hit" 
         first_hit = page.locator(".product-card").first
 
         first_hit.screenshot( path="eerste_iphone.png")
 
     finally:
-        # A place for teardown and cleanups. (Playwright handles browser closing)
         print("Automation finished!")
 
 
